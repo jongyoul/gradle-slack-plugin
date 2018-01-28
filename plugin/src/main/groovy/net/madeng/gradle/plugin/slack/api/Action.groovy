fd02766042b5dd716075ca5d3ab566171767a954
denuno <valliantster@gmail.com>
2018-01-28 14:23:13 -0700

    Add message attachment actions

---
+++ plugin/src/main/groovy/net/madeng/gradle/plugin/slack/api/Action.groovy
@@ -0,0 +1,31 @@
+package net.madeng.gradle.plugin.slack.api
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class Action {
+
+  Confirm confirm
+  String name;
+  String text;
+  String style;
+  String type;
+  String value;
+
+  def confirm(Closure closure) {
+    closure.resolveStrategy = Closure.DELEGATE_FIRST
+    confirm = new Confirm()
+    closure.delegate = confirm
+    closure()
+  }
+
+  class Confirm {
+    String title;
+    String text;
+    String okText;
+    String dismissText;
+  }
+
+}
+
